71e4b4640cc99d8b2f0be733b06cb223051cbc93
gayuni00 <ada9979@naver.com>
2023-09-19 19:10:57 +0900

    html 초기소스
    처음 공유합니다.

---
+++ src/main/resources/static/html_source/js/main.js
@@ -0,0 +1,133 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+})(jQuery);
+
+//shoppingbasket 장바구니
+function removeItem(itemId) {
+    document.getElementById(itemId).remove();
+}
+
+function changeQuantity(elementId, change) {
+    var quantityElement = document.getElementById(elementId);
+    var currentQuantity = parseInt(quantityElement.innerText);
+    var newQuantity = currentQuantity + change;
+
+    if (newQuantity >= 0) {
+        quantityElement.innerText = newQuantity;
+    }
+}
+
+//stock check 재고페이지
+function showPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "flex";
+}
+
+function hidePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
+
+function addToCart() {
+    // 여기에 실제로 장바구니에 상품을 추가하는 로직을 추가할 수 있습니다.
+    alert("상품이 장바구니에 담겼습니다.");
+    hidePopup();
+}
+
